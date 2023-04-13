902313f035425a5e725ba5e3c3e39dab90322945
rubykong <ru.kong@u.nus.edu>
2023-04-13 18:45:11 +0800

    change again to download data

--- src/cbig_network_correspondence/__init__.py
+++ src/cbig_network_correspondence/__init__.py
@@ -1,3 +1,13 @@
 from . import compute_overlap_with_atlases
 from . import visualize_overlap_lib
 from . import load_example
+from git import Repo
+from os import path
+
+data_dir = path.join(path.dirname(path.abspath(__file__)), 'data')
+if not path.exists(data_dir):
+    print("Download data")
+    Repo.clone_from('https://github.com/rubykong/cbig_network_correspondence_data', data_dir)
+else:
+    repo_dir = Repo(data_dir)
+    repo_dir.remotes.origin.pull('master')
--- src/cbig_network_correspondence/__install__.py
+++
@@ -1,9 +0,0 @@
-from git import Repo
-from os import path
-
-def main():
-    print("Download data")
-    Repo.clone_from('https://github.com/rubykong/cbig_network_correspondence_data', path.dirname(path.abspath(__file__)))
-    
-if __name__ == '__main__':
-    main()
