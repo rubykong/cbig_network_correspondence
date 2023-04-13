da48acff8e85f6264b55be85141944f9c5313a6f
rubykong <ru.kong@u.nus.edu>
2023-04-13 17:19:59 +0800

    put data repo to setup

--- src/cbig_network_correspondence/__init__.py
+++ src/cbig_network_correspondence/__init__.py
@@ -1,8 +1,3 @@
 from . import compute_overlap_with_atlases
 from . import visualize_overlap_lib
 from . import load_example
-import asyncio
-import os
-
-output_dir = os.path.dirname(os.path.abspath(__file__))
-os.system("gh-folder-download --url https://github.com/rubykong/cbig_network_correspondence_data/tree/master/data --output " + output_dir)
