7f41e1a55433732be421e02971ab2ea1ca837509
rubykong <ru.kong@u.nus.edu>
2023-04-13 14:52:41 +0800

    uncomment importing modules

--- src/cbig_network_correspondence/__init__.py
+++ src/cbig_network_correspondence/__init__.py
@@ -1,6 +1,6 @@
-#from . import compute_overlap_with_atlases
-#from . import visualize_overlap_lib
-#from . import load_example
+from . import compute_overlap_with_atlases
+from . import visualize_overlap_lib
+from . import load_example
 import asyncio
 import os
 
