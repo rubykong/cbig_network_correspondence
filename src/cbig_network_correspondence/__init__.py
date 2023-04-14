f9803c8767344b0ac8c0e41778a83cc029e8ad45
rubykong <ru.kong@u.nus.edu>
2023-04-14 13:59:14 +0800

    resolving windows and macOS git refresh issue

--- src/cbig_network_correspondence/__init__.py
+++ src/cbig_network_correspondence/__init__.py
@@ -1,10 +1,11 @@
 from . import compute_overlap_with_atlases
 from . import visualize_overlap_lib
 from . import load_example
+import os
+os.environ["GIT_PYTHON_REFRESH"] = "quiet"
 from git import Repo
-from os import path
 
-data_dir = path.join(path.dirname(path.abspath(__file__)), 'data')
+data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 if not path.exists(data_dir):
     print("Download data")
     Repo.clone_from('https://github.com/rubykong/cbig_network_correspondence_data', data_dir)
